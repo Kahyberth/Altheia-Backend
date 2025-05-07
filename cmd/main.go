51f96561c016c09c09cd9734959e468fedf9fca9
Kahyberth <kahyberth@gmail.com>
2025-05-07 13:05:31 -0500

    feat(auth): add refresh token support using singleton pattern

--- cmd/main.go
+++ cmd/main.go
@@ -3,19 +3,19 @@ package main
 import (
 	"Altheia-Backend/config"
 	"Altheia-Backend/internal/auth"
+	"Altheia-Backend/internal/db"
 	"Altheia-Backend/internal/middleware"
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
-	config.LoadEnv()
-	db := config.ConnectDB()
-	err := db.AutoMigrate(&auth.User{})
+	database := db.GetDB()
+	err := database.AutoMigrate(&auth.User{})
 	if err != nil {
 		return
 	}
 
-	authRepo := auth.NewRepository(db)
+	authRepo := auth.NewRepository(database)
 	authService := auth.NewService(authRepo)
 	authHandler := auth.NewHandler(authService)
 
@@ -24,6 +24,7 @@ func main() {
 	authGroup := app.Group("/auth")
 	authGroup.Post("/register", authHandler.Register)
 	authGroup.Post("/login", authHandler.Login)
+	authGroup.Post("/refresh-token/:refresh_token", authHandler.RefreshTokenH)
 
 	profile := app.Group("/profile")
 	profile.Use(middleware.JWTProtected())
